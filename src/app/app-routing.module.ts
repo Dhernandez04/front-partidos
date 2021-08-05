a5116932227eac2c81c2bbb9ba2c23db45ef89db
Oscar <oscarballestach@gmail.com>
2021-08-05 18:55:16 -0500

    Añadiendo rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrarComponent } from './auth/registrar/registrar.component';
+import { ListpartidoComponent } from './partidos/listpartido/listpartido.component';
+import { OpcionesComponent } from './partidos/opciones/opciones.component';
+import { UpmarcadorComponent } from './partidos/upmarcador/upmarcador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:LoginComponent,
+  pathMatch: 'full'},
+  {path: 'opciones', component:OpcionesComponent},
+  {path: 'lista', component:ListpartidoComponent},
+  {path: 'actualizar', component:UpmarcadorComponent},
+  {path: 'registrar', component:RegistrarComponent},
+  {path:"**", redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

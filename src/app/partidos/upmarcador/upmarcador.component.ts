7af8634dc8ca15c73e48deb84ba23a5ea9c58986
deimer <deimercode@gmail.com>
2021-08-06 11:14:57 -0500

    agregando componente para actualizar el marcador

--- src/app/partidos/upmarcador/upmarcador.component.ts
+++ src/app/partidos/upmarcador/upmarcador.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { forkJoin, Observable } from 'rxjs';
 import { Equipo } from 'src/app/models/Equipo';
 import { Partido } from 'src/app/models/partido';
 import { EquipoService } from 'src/app/services/equipo.service';
@@ -42,35 +43,46 @@ export class UpmarcadorComponent implements OnInit {
   actualizar(){
     console.log(this.actualizarForm.value)
        this.partidoService.actualiarPartido(this.id,this.actualizarForm.value).subscribe((res)=>{
-          
           this.router.navigateByUrl('/lista')
        })
   }
   traerPartido(id:number){
+    
     this.partidoService.traerUno(id).subscribe((partido: any) => {
       this.partido = partido.equipos;
-      //console.log(this.partido);
-      this.traervisitante(this.partido.visitante)
-      this.traerlocal(this.partido.local)
-      this.actualizarForm.setValue({
-        fecha: this.partido.fecha,
-        local: this.local.id,
-        visitante: this.visitante.id
-       })
+      const observable = forkJoin({
+        obs1: this.traervisitante(this.partido.visitante),
+        obs2: this.traerlocal(this.partido.local),
+  
+      });
+      observable.subscribe((value:any)=>{
+        console.log(value);
+        this.local = value.obs1.equipos;
+        this.visitante = value.obs2.equipos
+         this.actualizarForm.setValue({
+
+            fecha: partido.equipos.fecha,
+            local:value.obs1.equipos.id,
+            visitante:value.obs2.equipos.id,
+            goles_local: 0,
+            goles_visitante:0
+           })
+      })
+      // 
+      // console.log(this.partido);
+     
+      
+      // console.log(this.local);
+      
+      
        })
       }
   
   traervisitante(id:number){
-    this.equipoService.traerUno(id).subscribe((equipo: any) => {
-      this.visitante = equipo.equipos;
-      console.log(this.visitante);
-    })
+  return this.equipoService.traerUno(id);
   }
 
   traerlocal(id:number){
-    this.equipoService.traerUno(id).subscribe((equipo: any) => {
-      this.local = equipo.equipos;
-      console.log(this.local);
-    })
+    return this.equipoService.traerUno(id);
   }
 }
